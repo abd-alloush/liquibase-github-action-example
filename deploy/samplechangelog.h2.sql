ecf592755f2142e4c883f27fc871994502798683
Abd Alloush <abdalloush@ip-10-0-0-199.ec2.internal>
2022-02-10 17:06:33 -0500

    Create Master File & Deploy on only dev env workflow

---
+++ deploy/samplechangelog.h2.sql
@@ -0,0 +1,40 @@
+--liquibase formatted sql
+
+--changeset abd-alloush:1
+--rollback DROP TABLE person;
+create table person (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+
+--changeset abd-alloush:2
+--rollback DROP TABLE company;
+create table company (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+
+--changeset abd-alloush:3
+--rollback DROP TABLE yourname;
+CREATE TABLE testtable (
+    id int primary key,
+    name varchar(50) not null
+)
+
+--changeset other.dev:4
+--rollback ALTER TABLE person DROP COLUMN country;
+alter table person add column country varchar(2)
+
+--changeset other.dev:5
+--rollback ALTER TABLE person DROP COLUMN state;
+alter table person add column state varchar(2)
+
+--changeset other.dev:6
+--rollback ALTER TABLE company DROP COLUMN country;
+alter table company add column country varchar(2)
