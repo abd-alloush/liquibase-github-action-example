3559ceaecf9fca4aef634c381c19ed3bd4f2ccd7
Abd Alloush <abdalloush@Abds-MacBook-Pro.local>
2022-02-11 10:07:46 -0500

    Two steps for same job, update & history + debug enabled

--- deploy/run-sql-on-dev.sql
+++ deploy/run-sql-on-dev.sql
@@ -4,3 +4,17 @@ CREATE TABLE testtable123 (
     id int primary key,
     name varchar(50) not null
 )
+
+--changeset abd-alloush:9
+--rollback DROP TABLE yourname;
+CREATE TABLE newtable (
+    id int primary key,
+    name varchar(50) not null
+)
+
+--changeset abd-alloush:10
+--rollback DROP TABLE yourname;
+CREATE TABLE newtable (
+    id int primary key,
+    name varchar(50) not null
+)
